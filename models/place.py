93d1c61b82225a42800d25c45d569a5a48626070
miguel5219 <5219@holbertonstudents.com>
2022-12-08 12:28:53 -0500

    update

--- models/place.py
+++ models/place.py
@@ -9,10 +9,11 @@ from models.review import Review
 
 place_amenity = Table('place_amenity', Base.metadata,
                         Column("amenity_id", String(60), ForeignKey(
-                                "amenities.id"), primary_key=True, nullable=False),
+                            "amenities.id"), primary_key=True, nullable=False),
                         Column("place_id", String(60), ForeignKey("places.id"),
-                                primary_key=True, nullable=False),
+                            primary_key=True, nullable=False),
                         extend_existing=True)
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
