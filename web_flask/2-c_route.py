f32073a361d1f1de8b43fff447b662bf084e4530
miguel5219 <5219@holbertonstudents.com>
2022-12-09 15:11:36 -0500

    Write a script that starts a Flask web application

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_route():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strcit_slashes=False)
+def C_route(text):
+    newText = text.replace('_', ' ')
+    return 'C {}'.format(escape(newText))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
