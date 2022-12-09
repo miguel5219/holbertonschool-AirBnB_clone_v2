81570b418fcfdabecaa6a37d31c03619d086f807
miguel5219 <5219@holbertonstudents.com>
2022-12-09 12:11:52 -0500

    Update basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -59,11 +59,12 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
 
-        if '_sa_instance_state' in dictionary:
+        if '_sa_instance_state' in dictionary.keys():
             del dictionary['_sa_instance_state']
         return dictionary
 
     def delete(self):
         """ delete the current instance """
         from models import storage
-        storage.delete(self)
+        for key in storage.__objects.keys():
+            storage.delete(storage.__objects[key])
