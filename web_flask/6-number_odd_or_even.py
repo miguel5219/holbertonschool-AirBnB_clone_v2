7381afc6f2ec9f502a5ea79464aa082690a6a631
miguel5219 <5219@holbertonstudents.com>
2022-12-09 16:33:20 -0500

    update

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask
+from markupsafe import escape
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+    """ func to display 'Hello HBNB!. """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_route():
+    """ func to display 'HBNB' """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def C_route(text):
+    """ display “C ” followed by the value of the text variable """
+    newText = text.replace('_', ' ')
+    return 'C {}'.format(escape(newText))
+
+
+@app.route("/python/", defaults={'text': 'is cool'})
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text):
+    """ . """
+    newText = text.replace('_', ' ')
+    return 'Python {}'.format(escape(newText))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_Route(n):
+    """ . """
+    return '{} is a number'. format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template_route(n):
+    """ . """
+    return render_template('5-number.html', n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
