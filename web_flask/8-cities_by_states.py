453f1f61ce3abf69e47b7c3d51c862a9dd728b1f
miguel5219 <5219@holbertonstudents.com>
2022-12-10 17:43:19 -0500

    update

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" starts a Flask web application """
+
+from flask import Flask
+from markupsafe import escape
+from flask import render_template
+from models import storage
+from models.state import State
+from models.city import City
+from os import getenv
+from sqlalchemy.orm import relationship
+
+app = Flask(__name__)
+
+
+@app.route('cities_by_states', strict_slashes=False)
+def cities_by_state():
+    from models.state import State
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities_ob = [c for c in list(storage.all(City).values())]
+        states_ob = [s for s in storage.all(State).values()]
+        return render_template("8_cities_by_states.html",
+                               states_ob=states_ob, cities_ob=cities_ob)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
