0ff9d6efc897b954c9532eb2aebe39892e885085
miguel5219 <5219@holbertonstudents.com>
2022-12-12 11:18:13 -0500

    update

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -5,38 +5,33 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.city import City
-from os import getenv
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
 def states_route():
+    """ displa HTML page """
     states_ob = [s for s in storage.all(State).values()]
     return render_template("9-states.html", states_ob=states_ob)
 
 
-@app.route('/states', strict_slashes=False)
-def states_route():
-    """ show a HTML page """
-    _states = [state for state in storage.all(State).values()]
-    return render_template(
-        '9-states.html', _states=_states)
-
-
 @app.route('/states/<id>', strict_slashes=False)
 def states_route_id(id):
+    """ display HTML page """
     states_ob = [s for s in storage.all(State).values()]
     states_ids = [s.id for s in storage.all(State).values()]
     cities_ob = [c for c in list(storage.all(City).values())]
     return render_template("9-states.html",
-                               cities_ob=cities_ob,
-                               states_ob=states_ob,
-                               id=id, states_ids=states_ids)
+                           cities_ob=cities_ob,
+                           states_ob=states_ob,
+                           id=id, states_ids=states_ids)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
+    """ close the current SQLAlchemy session """
     storage.close()
 
 
