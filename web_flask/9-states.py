3ccc610144c6347a4f63fa666f310c7c8bf1a5f7
miguel5219 <5219@holbertonstudents.com>
2022-12-12 11:55:30 -0500

    .

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" starts a Flask web application """
-
+""" Flask: rendering template """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -10,29 +9,31 @@ import os
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def teardown(exception):
+    """ removes the current SQLAlchemy Session """
+    storage.close()
+
+
 @app.route('/states', strict_slashes=False)
 def states_route():
-    """ displa HTML page """
-    _states = [state for state in storage.all(State).values()]
-    return render_template("9-states.html", _states=_states)
+    """ displays a HTML page """
+    states = [state for state in storage.all(State).values()]
+    return render_template(
+            '9-states.html', states=states)
 
 
 @app.route('/states/<id>', strict_slashes=False)
-def states_route_id(id):
-    """ display HTML page """
-    _states = [state for state in storage.all(State).values()]
+def state_id(id):
+    """ displays a HTML page """
+    states = [state for state in storage.all(State).values()]
     list_ids = [state.id for state in storage.all(State).values()]
-    _cities = [city for city in list(storage.all(City).values())]
-    return render_template("9-states.html",
-                           _cities=_cities,
-                           _states=_states,
-                           id=id, list_ids=list_ids)
-
-
-@app.teardown_appcontext
-def teardown_db(exception):
-    """ close the current SQLAlchemy session """
-    storage.close()
+    cities = [city for city in list(storage.all(City).values())]
+    return render_template(
+        '9-states.html',
+        cities=cities,
+        states=states,
+        id=id, list_ids=list_ids)
 
 
 if __name__ == '__main__':
